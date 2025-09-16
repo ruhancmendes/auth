b272369720bebb9509a3b9858caa9361d9dc7994
ruhancmendes <ruhancmendes@gmail.com>
2025-09-16 17:50:53 -0300

    create session

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+    const { username, password } = request.body
+
+    const fakeUser = {
+      id: 1,
+      username: "Ruhan",
+      password: "123456",
+    }
+
+    if(username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username or password incorrect", 401)
+    }
+
+    const { secret } = authConfig.jwt
+    const token = sign({}, secret, {
+      expiresIn: "1d",
+      subject: String(fakeUser.id),
+    })
+
+    return response.json({ token })
   }
 }
 
